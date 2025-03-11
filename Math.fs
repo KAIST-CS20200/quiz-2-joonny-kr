ac475532459d411a6f3a96edba29b5b0c9e42fa5
Youngjoon Cho <yjcho4295@gmail.com>
2025-03-11 10:25:11 +0900

    Update Math

--- Math.fs
+++ Math.fs
@@ -6,4 +6,11 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  let N = uint64 n
+  let rec operate N count=
+    if N = 1UL then count
+    elif N % 2UL = 0UL then
+      operate (N / 2UL) (count + 1)
+    else
+      operate (3UL * N + 1UL) (count + 1)
+  operate N 0
